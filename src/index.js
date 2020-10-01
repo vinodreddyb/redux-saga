ee5d040e9be3c81cd34abe8a70b3b8bdcdbc868c
vinodreddyb <vinodbatthula@gmail.com>
2020-10-01 10:17:02 +0530

    test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios'
+import {createStore, applyMiddleware} from "redux";
+import reducers from "./reducers"
+import {Provider} from "react-redux";
+import createSagaMiddleware from 'redux-saga'
+import rootSaga from "./sagas";
+import {BrowserRouter} from "react-router-dom";
 
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = "http://rem-rest-api.herokuapp.com/api"
+
+const sagaMiddleWare = createSagaMiddleware();
+const store = createStore(reducers,applyMiddleware(sagaMiddleWare))
+sagaMiddleWare.run(rootSaga)
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}> <BrowserRouter><App /> </BrowserRouter></Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
